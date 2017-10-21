8de5ed0ac52cc739ce0eb2d6ed31dc7b902bb279
johnprada <32979609+johnprada@users.noreply.github.com>
2017-10-21 19:54:33 +0900

    Add files via upload

---
+++ SMART_UNITY3D.ino
@@ -0,0 +1,212 @@
+// The SFE_LSM9DS0 requires both the SPI and Wire libraries.
+// Unfortunately, you'll need to include both in the Arduino
+// sketch, before including the SFE_LSM9DS0 library.
+#include <SPI.h> // Included for SFE_LSM9DS0 library
+#include <Wire.h>
+#include <SFE_LSM9DS0.h>
+//#include "Arduino.h"
+//#include <Adafruit_GFX.h>
+//#include <Adafruit_PCD8544.h>
+#include "Timer.h"
+Timer t1,t2,t3;
+
+///////////////////////
+// Example I2C Setup //
+///////////////////////
+// Comment out this section if you're using SPI
+// SDO_XM and SDO_G are both grounded, so our addresses are:
+//#define LSM9DS0_XM  0x1D // Would be 0x1E if SDO_XM is LOW
+//#define LSM9DS0_G   0x6B // Would be 0x6A if SDO_G is LOW
+// Create an instance of the LSM9DS0 library called `dof` the
+// parameters for this constructor are:
+// [SPI or I2C Mode declaration],[gyro I2C address],[xm I2C add.]
+LSM9DS0 dof(MODE_I2C, 0x6B, 0x1D);
+
+///////////////////////
+// Example SPI Setup //
+///////////////////////
+/* // Uncomment this section if you're using SPI
+#define LSM9DS0_CSG  9  // CSG connected to Arduino pin 9
+#define LSM9DS0_CSXM 10 // CSXM connected to Arduino pin 10
+LSM9DS0 dof(MODE_SPI, LSM9DS0_CSG, LSM9DS0_CSXM);
+*/
+
+///////////////////////////////
+// Interrupt Pin Definitions //
+///////////////////////////////
+const byte INT1XM = 3; // INT1XM tells us when accel data is ready
+const byte INT2XM = 2; // INT2XM tells us when mag data is ready
+const byte DRDYG  = 4; // DRDYG  tells us when gyro data is ready
+
+// global constants for 9 DoF fusion and AHRS (Attitude and Heading Reference System)
+#define GyroMeasError PI * (40.0f / 180.0f)       // gyroscope measurement error in rads/s (shown as 3 deg/s)
+#define GyroMeasDrift PI * (0.0f / 180.0f)      // gyroscope measurement drift in rad/s/s (shown as 0.0 deg/s/s)
+// There is a tradeoff in the beta parameter between accuracy and response speed.
+// In the original Madgwick study, beta of 0.041 (corresponding to GyroMeasError of 2.7 degrees/s) was found to give optimal accuracy.
+// However, with this value, the LSM9SD0 response time is about 10 seconds to a stable initial quaternion.
+// Subsequent changes also require a longish lag time to a stable output, not fast enough for a quadcopter or robot car!
+// By increasing beta (GyroMeasError) by about a factor of fifteen, the response time constant is reduced to ~2 sec
+// I haven't noticed any reduction in solution accuracy. This is essentially the I coefficient in a PID control sense; 
+// the bigger the feedback coefficient, the faster the solution converges, usually at the expense of accuracy. 
+// In any case, this is the free parameter in the Madgwick filtering and fusion scheme.
+#define beta sqrt(3.0f / 4.0f) * GyroMeasError   // compute beta
+#define zeta sqrt(3.0f / 4.0f) * GyroMeasDrift   // compute zeta, the other free parameter in the Madgwick scheme usually set to a small or zero value
+
+
+float pitch, yaw, roll, heading;
+
+float abias[3] = {0, 0, 0}, gbias[3] = {0, 0, 0};
+float ax, ay, az, gx, gy, gz, mx, my, mz; // variables to hold latest sensor data values 
+float temperature;
+int data;
+void setup()
+{
+  Serial.begin(38400); // Start serial at 38400 bps
+ 
+  // Set up interrupt pins as inputs:
+  pinMode(INT1XM, INPUT);
+  pinMode(INT2XM, INPUT);
+  pinMode(DRDYG,  INPUT);
+
+
+  uint32_t status = dof.begin();
+ 
+
+ // Set data output ranges; choose lowest ranges for maximum resolution
+ // Accelerometer scale can be: A_SCALE_2G, A_SCALE_4G, A_SCALE_6G, A_SCALE_8G, or A_SCALE_16G   
+    dof.setAccelScale(dof.A_SCALE_2G);
+ // Gyro scale can be:  G_SCALE__245, G_SCALE__500, or G_SCALE__2000DPS
+    dof.setGyroScale(dof.G_SCALE_245DPS);
+ // Magnetometer scale can be: M_SCALE_2GS, M_SCALE_4GS, M_SCALE_8GS, M_SCALE_12GS   
+    dof.setMagScale(dof.M_SCALE_4GS);
+  
+ // Set output data rates  
+ // Accelerometer output data rate (ODR) can be: A_ODR_3125 (3.225 Hz), A_ODR_625 (6.25 Hz), A_ODR_125 (12.5 Hz), A_ODR_25, A_ODR_50, 
+ //                                              A_ODR_100,  A_ODR_200, A_ODR_400, A_ODR_800, A_ODR_1600 (1600 Hz)
+    dof.setAccelODR(dof.A_ODR_200); // Set accelerometer update rate at 100 Hz
+ // Accelerometer anti-aliasing filter rate can be 50, 194, 362, or 763 Hz
+ // Anti-aliasing acts like a low-pass filter allowing oversampling of accelerometer and rejection of high-frequency spurious noise.
+ // Strategy here is to effectively oversample accelerometer at 100 Hz and use a 50 Hz anti-aliasing (low-pass) filter frequency
+ // to get a smooth ~150 Hz filter update rate
+    dof.setAccelABW(dof.A_ABW_50); // Choose lowest filter setting for low noise
+ 
+ // Gyro output data rates can be: 95 Hz (bandwidth 12.5 or 25 Hz), 190 Hz (bandwidth 12.5, 25, 50, or 70 Hz)
+ //                                 380 Hz (bandwidth 20, 25, 50, 100 Hz), or 760 Hz (bandwidth 30, 35, 50, 100 Hz)
+    dof.setGyroODR(dof.G_ODR_190_BW_125);  // Set gyro update rate to 190 Hz with the smallest bandwidth for low noise
+
+ // Magnetometer output data rate can be: 3.125 (ODR_3125), 6.25 (ODR_625), 12.5 (ODR_125), 25, 50, or 100 Hz
+    dof.setMagODR(dof.M_ODR_125); // Set magnetometer to update every 80 ms
+    
+ // Use the FIFO mode to average accelerometer and gyro readings to calculate the biases, which can then be removed from
+ // all subsequent measurements.
+    dof.calLSM9DS0(gbias, abias);
+    t1.every(100, printHeading);
+    t2.every(100, printOrientation);
+//    t3.every(200, SentFromUnity);
+}
+
+void loop()
+{
+  if(digitalRead(DRDYG)) {  // When new gyro data is ready
+  dof.readGyro();           // Read raw gyro data
+    gx = dof.calcGyro(dof.gx) - gbias[0];   // Convert to degrees per seconds, remove gyro biases
+    gy = dof.calcGyro(dof.gy) - gbias[1];
+    gz = dof.calcGyro(dof.gz) - gbias[2];
+  }
+  
+  if(digitalRead(INT1XM)) {  // When new accelerometer data is ready
+    dof.readAccel();         // Read raw accelerometer data
+    ax = dof.calcAccel(dof.ax) - abias[0];   // Convert to g's, remove accelerometer biases
+    ay = dof.calcAccel(dof.ay) - abias[1];
+    az = dof.calcAccel(dof.az) - abias[2];
+  }
+  
+  if(digitalRead(INT2XM)) {  // When new magnetometer data is ready
+    dof.readMag();           // Read raw magnetometer data
+    mx = dof.calcMag(dof.mx);     // Convert to Gauss and correct for calibration
+    my = dof.calcMag(dof.my);
+    mz = dof.calcMag(dof.mz);
+    
+    dof.readTemp();
+    temperature = 21.0 + (float) dof.temperature/8.; // slope is 8 LSB per degree C, just guessing at the intercept
+  }
+t1.update();
+t2.update();
+//t3.update();
+  SentFromUnity();
+ 
+ 
+//    printHeading(mx, my);
+//    printOrientation(ax, ay, az);
+//    
+}
+
+
+void printHeading()
+{
+  if (my > 0)
+  {
+    heading = 90 - (atan(mx / my) * (180 / PI));
+  }
+  else if (my < 0)
+  {
+    heading = - (atan(mx / my) * (180 / PI));
+  }
+  else // my = 0
+  {
+    if (mx < 0) heading = 180;
+    else heading = 0;
+  }
+  heading=map(heading,144,100,90,180);
+ //Serial.print("Heading: ");
+ //Serial.println(heading, 2);
+}
+
+
+void printOrientation()
+{
+ // float pitch, roll;
+  
+  pitch = atan2(ax, sqrt(ay * ay) + (az * az));
+  roll = atan2(ay, sqrt(ax *ax) + (az * az));
+  pitch *= 180.0 / PI;
+  roll *= 180.0 / PI;
+ // pitch=map(pitch,-90,90,0,180);
+   roll=map(roll,-90,90,0,180);
+//  Serial.print("Roll: ");
+//  Serial.print("Pitch, Roll: ");
+  //Serial.println(pitch, 2);
+//  Serial.print(", ");
+ //Serial.println(roll, 2);  
+//delay(100);
+
+}
+
+
+void SentFromUnity()
+  {
+    
+    if (Serial.available())
+    {
+      data=Serial.read();
+      if (data=='a')//heading or yaw
+      {
+         Serial.write(heading);
+        }
+      
+      if (data=='b')//Pitch
+      {
+        Serial.write(pitch);     
+        }
+        if (data=='c')//Roll
+      {
+        Serial.write(roll);     
+        }
+      }
+  }
+
+  
+  
+  
+
+
