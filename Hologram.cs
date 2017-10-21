e0c1bbaa64b1333a3feaa2c577cad2de9a9b3d1a
johnprada <32979609+johnprada@users.noreply.github.com>
2017-10-21 19:56:54 +0900

    Add files via upload

---
+++ Hologram.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hologram : MonoBehaviour {
+
+	public GameObject Model ;
+	public float xF,yF,zF;
+	public bool SetPosition;
+	public bool ZoomSMART;
+
+	// Use this for initialization
+
+
+	// Update is called once per frame
+	void Update () {
+		float[] Posx = new float [1];
+		float[] Posy = new float [1];
+		float[] Posz = new float [1];
+//		float[] Posxx = new float [1];
+//		float[] Posyy = new float [1];
+//		float[] Poszz = new float [1];
+
+		xF = (float)Model.transform.eulerAngles.x;
+		yF = (float) Model.transform.eulerAngles.y;
+		zF = (float)Model.transform.eulerAngles.z;
+//				xxF = (float)Model.transform.rotation.x;
+//				yyF = (float) Model.transform.rotation.y;
+//				zzF = (float)Model.transform.rotation.z;
+		Posx[0] = xF;
+		Posy [0]= yF;
+		Posz [0]= zF;
+//		Posxx[0] = xxF;
+//		Posyy [0]= yyF;
+//		Poszz [0]= zzF;
+		transform.position= Model.transform.position;
+        transform.localScale = new Vector3(0.01F, 0.01F, 0.01F);
+        if (SetPosition==true){
+			transform.eulerAngles = new Vector3 (Posx[0],Posy[0],Posz[0]);//y 0 z
+			transform.rotation= Model.transform.rotation;
+			transform.position= Model.transform.position;
+		}
+		if( ZoomSMART==true)
+            transform.localScale = new Vector3(0.02F, 0.02F, 0.02F);
+    }
+}
---
+++ Sensor.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Sensor : MonoBehaviour {
+
+    public float x, y, z;
+    public double yaw, pitch, roll, xF, yF, zF;
+    public Quaternion MyRotation = Quaternion.identity;
+    public bool ZoomSMART, SensitivityX2, SaveData, StartExperiment;
+    //public float yRotation = 5.0F;
+    private int j, i, h;
+    SerialPort serial = new SerialPort("COM3", 250000);//7 3
+
+    void Start() {
+        /*serial.Open();
+        serial.ReadTimeout = 50;//50*/
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+         
+
+		{
+			try
+			{
+
+				//sp.Write("0");
+				//print(serial.ReadByte());//esto imprime las opciones en arduino
+			
+				/*serial.Write ("a");
+				GetdataFunctionPitch(serial.ReadByte());
+				serial.Write("b");
+				GetdataFunctionYaw(serial.ReadByte());*/
+
+
+
+
+			}
+			catch(System.Exception)
+			{	
+			}
+
+//			x = x / 10;
+//			y = y / 10;
+			//yRotation += Input.GetAxis ("Horizontal");
+			transform.eulerAngles = new Vector3 (y,x, 0);//y 0 z
+			MyRotation.eulerAngles = new Vector3 (y,x, 0);//y 0 z
+			transform.localScale = new Vector3 (0.01F, 0.01F, 0.01F);
+			yaw   = (double) Mathf.Atan2(2.0f * (MyRotation.y*MyRotation.z+ MyRotation.x * MyRotation.w), MyRotation.x *MyRotation.x + MyRotation.y * MyRotation.y - MyRotation.z* MyRotation.z - MyRotation.w * MyRotation.w)*Mathf.Rad2Deg;   
+			pitch = (double) -Mathf.Asin(2.0f * (MyRotation.y * MyRotation.w - MyRotation.x * MyRotation.z))*Mathf.Rad2Deg;
+			roll  = (double) Mathf.Atan2(2.0f * (MyRotation.x  * MyRotation.y + MyRotation.z * MyRotation.w), MyRotation.x  * MyRotation.x - MyRotation.y  * MyRotation.y  - MyRotation.z * MyRotation.z  + MyRotation.w  * MyRotation.w)*Mathf.Rad2Deg; 			//print(transform.eulerAngles.x);
+			if (yaw < 0)
+				yaw = yaw * -1;
+			if (pitch < 0)
+			pitch = pitch * -1;
+			if (roll < 0)
+				roll = roll * -1;
+			if (SaveData == true)
+				Txt_Data ();
+			if (ZoomSMART == true)
+			transform.localScale = new Vector3 (0.02F, 0.02F, 0.02F);
+			if (SensitivityX2 == true){
+				transform.eulerAngles = new Vector3 ((y)*2F,(x)*2F, 0);//y 0 z
+				MyRotation.eulerAngles = new Vector3 ((y)*2F,(x)*2F, 0);//y 0 z
+
+			}
+			if (StartExperiment == true) {
+				Debug.Log ("Start Experiment");
+				StartExperiment = false;
+			}
+			xF = (double) MyRotation.eulerAngles.x*Mathf.Rad2Deg;;
+			yF = (double) MyRotation.eulerAngles.y*Mathf.Rad2Deg;;
+			
+
+		}	//if (!serial.IsOpen)
+
+
+			}
+	void GetdataFunctionPitch(float data){
+		x = data;
+
+
+
+
+
+
+	}
+	void GetdataFunctionYaw(float data){
+		y = data;
+
+
+
+		}
+	
+	void Txt_Data()
+	{
+		 
+		Debug.Log(pitch+"  "+yaw+"  "+roll+"  "+xF+"  "+yF+"  "+zF+"  "+MyRotation.x+"  "+MyRotation.y+"  "+MyRotation.z );//File.Write(xx[i].ToString()+"\r\n");
+//		print(transform.localRotation.x);
+       
+	}
+		//File.Close();
+//		StreamWriter File = new StreamWriter ("TestTest1.txt");
+//
+//		SortedList<float,int> output = new SortedList<float,int> (){};
+//		if (SaveData == true) {
+//
+//			j++;		
+//			output.Add (x, j);
+//				
+//
+//		}
+//		foreach (KeyValuePair<float,int> listItem in output) {
+//			File.WriteLine(listItem.ToString () + "\r\n");
+//
+//		}
+//		File.Close();//
+//		foreach (float listItem in output) {
+//						File.Write(listItem.ToString()+"\r\n");
+//		}
+
+
+//		float [] output= new float[N];
+//		for ( i = 0; i < output.Count; i++) 
+//		{
+//			output [i] = x;
+//			File.Write(output[i].ToString()+"\r\n");
+//		}
+//
+//		File.Close();
+
+
+    
+
+
+}
+
+    
